7ae1c034ba057448b1634ebc4da7bceb64c7a9c4
Paul Bouhours <bouhours.paul@gmail.com>
2021-07-22 22:21:50 +0200

    change cors

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 // TODO: configure cors
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // TODO: add your routes here
 
